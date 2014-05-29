0a52cfe1f109bf6e1d98efa6774596ac176fa27b
Ted Lawless <lawlesst@gmail.com>
2014-05-29 13:07:32 -0400

    Initial.

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '15a620d7856f91dda2a317e110e50812058529422576fe402c9cdc7803109d64ba1cfe88a85ab6f8acd99d0da4fbc27cb797315bf638bf230a5718075ecbde9c'
+
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BulSearch::Application.config.session_store :cookie_store, key: '_bul-search_session'
