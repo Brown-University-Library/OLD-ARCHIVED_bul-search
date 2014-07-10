6534d6175253d84ac548ec3e1f17422beafc1cd7
Ted Lawless <lawlesst@gmail.com>
2014-07-10 18:57:55 -0400

    Commit script.

--- data/clear_index.sh
+++ data/clear_index.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-SOLR_URL='http://localhost:8983/solr/blacklight-core'
+SOLR_URL='http://localhost:8081/solr/blacklight-core'
 
 curl $SOLR_URL/update?commit=true -H "Content-Type: text/xml" --data-binary '<delete><query>*:*</query></delete>'
