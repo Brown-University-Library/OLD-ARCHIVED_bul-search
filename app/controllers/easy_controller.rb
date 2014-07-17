da6039f1831cfb436d0516dded09f45a6bc55255
Ted Lawless <lawlesst@gmail.com>
2014-07-17 10:21:44 -0400

    Search box; routing.

--- app/controllers/easy_controller.rb
+++ app/controllers/easy_controller.rb
@@ -2,6 +2,12 @@
 
 class EasyController < ApplicationController
   def home
+    @easy_search = true
+    if params[:q].blank?
+        @has_query = false
+    else
+        @has_query = true
+    end
   end
 
   def search
@@ -9,5 +15,13 @@ class EasyController < ApplicationController
     #response.headers['Content-Type'] = 'text/javascript'
     render json: @search_result.to_json
     #body: @search_result.to_json, layout: false, content_type: "text/javascript"
+  end
+
+  def search_action_url
+      url_for(:controller => 'easy', :action => 'home', :only_path => true)
+  end
+
+  def options_for_select
+    return []
   end
 end
