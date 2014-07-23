e347974b09629197459c1c58dc01a9708b7cd98c
Ted Lawless <lawlesst@gmail.com>
2014-07-23 11:26:40 -0400

    Catalog record view route and spinners.

--- app/helpers/easy_helper.rb
+++ app/helpers/easy_helper.rb
@@ -2,8 +2,10 @@ module EasyHelper
   def data_link(query)
     url_for :controller=>'easy', :action=> 'search', :q => query
   end
-  def app_url
-    url_for :controller=>'catalog'
-    #return 'bobo'
+  def catalog_record_url
+    #This shouldn't be hard coded but couldn't find Blacklight documentation
+    #at first pass.
+    base = url_for :controller=>'catalog'
+    return base + 'catalog/'
   end
 end
