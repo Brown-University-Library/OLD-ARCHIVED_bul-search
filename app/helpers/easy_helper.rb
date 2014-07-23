597b87bfea989980b318dfe3aee6842d42a0b1ff
Ted Lawless <lawlesst@gmail.com>
2014-07-23 10:56:29 -0400

    Add helper for catalog url.

--- app/helpers/easy_helper.rb
+++ app/helpers/easy_helper.rb
@@ -2,4 +2,8 @@ module EasyHelper
   def data_link(query)
     url_for :controller=>'easy', :action=> 'search', :q => query
   end
+  def app_url
+    url_for :controller=>'catalog'
+    #return 'bobo'
+  end
 end
