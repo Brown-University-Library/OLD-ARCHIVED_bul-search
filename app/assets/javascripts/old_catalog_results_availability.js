8198a67b5a1551a3dbb175a94197bdf216c9140f
birkin <birkin.diana@gmail.com>
2014-08-18 08:44:55 -0400

    refactors results js to handle event issue.

--- app/assets/javascripts/old_catalog_results_availability.js
+++ app/assets/javascripts/old_catalog_results_availability.js
@@ -1,6 +1,5 @@
 /*
 - Grabs item divs, and for each item...
-  - Checks that availability info hasn't already been created (addressing a Safari event issue)
   - Gets bib_id
   - Hits availability api.
   - Determines summary availability.
@@ -15,16 +14,13 @@ $(document).on(  // $(document).ready... is problematic, see <http://guides.ruby
 );
 
 function grabItemDivs() {
-  /* Triggers ajax call for every item-div if necessary.
+  /* Triggers ajax call for every item-div.
    * Called on doc.on() */
   the_docs = $( ".document" );
   for (var i = 0; i < the_docs.length; i++) {
     the_doc = the_docs[i];
-    target_span = $( ".summary_availability", the_doc );  // check to see if the dom has already been updated (Safari event issue)
-    if ( target_span.length == 0 ) {
-      bib_id = $( "form", the_doc ).attr( "data-doc-id" );
-      getAvailabilityData( the_doc, bib_id );
-    }
+    bib_id = $( "form", the_doc ).attr( "data-doc-id" );
+    getAvailabilityData( the_doc, bib_id );
   }
 }
 
@@ -35,33 +31,37 @@ function getAvailabilityData( the_doc, bib_id ) {
   $.getJSON(
     api_url,
     function( response_object, success_status, ajax_object ) {  // these 3 vars are auto-created by $.getJSON; we only care about the response_object...
-      determineAvailability( response_object, the_doc );  // ...and our `the_doc`, which requires the anonymous function syntax to pass it along
+      populateDiv( response_object, the_doc );  // ...and our `the_doc`, which requires the anonymous function syntax to pass it along
     }
   );
 }
 
-function determineAvailability( response_object, the_doc ) {
-  /* Determines item's summarized availability; triggers html creation.
+function populateDiv( response_object, the_doc) {
+  /* Determines item's overall availability and updates html.
    * Called by getAvailabilityData() */
   var availability_status = "unknown";
-  for ( var index_key in response_object["items"] ){
-    item = response_object["items"][index_key]
-    if ( item["is_available"] == true ) {
-      availability_status = "available";
-      break;
-    } else if ( item["is_available"] == false ) {
-      availability_status = "unavailable";
-    }
+  if (response_object['items'].length > 0 ) {
+    availability_status = checkAvailability( response_object );
   }
-  populateDiv( the_doc, availability_status );
-}
-
-function populateDiv( the_doc, availability_status ) {
-  /* Builds and updates html.
-   * Called by getAvailabilityData() */
   availability_html = buildAvailabilityHtml( availability_status );
   $( the_doc ).append( availability_html );
   // $( the_doc ).append( "<pre>" + JSON.stringify(response_object, undefined, 2) + "</pre>" );
+}
+
+function checkAvailability( response_object ) {
+  /* Determines and returns item's summarized availability.
+   * Called by populateDiv() */
+  var return_status = "init";
+  for ( var index_key in response_object["items"] ){
+    item = response_object["items"][index_key]
+    if ( item["is_available"] == true ) {
+      return_status = "available";
+      break;
+    } else if ( item["is_available"] == false ) {
+      return_status = "unavailable";
+    }
+  }
+  return return_status;
 }
 
 function buildAvailabilityHtml( availability_status ) {
