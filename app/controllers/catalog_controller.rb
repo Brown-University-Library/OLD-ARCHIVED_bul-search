d862ba6fc91890d09ceee353a3deddf2b787449f
Ted Lawless <lawlesst@gmail.com>
2014-08-22 14:54:51 -0400

    Add access and location facet.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -56,6 +56,8 @@ class CatalogController < ApplicationController
     #
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
+    config.add_facet_field 'access_facet', :label => 'Access'
+    config.add_facet_field 'building_facet', :label => 'Location'
     config.add_facet_field 'format', :label => 'Format'
     config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
     config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
