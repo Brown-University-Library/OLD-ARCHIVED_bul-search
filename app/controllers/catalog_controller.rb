e2dcf2076910abd4c612ccbe40460329088e06f1
Ted Lawless <lawlesst@gmail.com>
2014-09-02 10:20:39 -0400

    Pull topic facet.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -60,7 +60,7 @@ class CatalogController < ApplicationController
     config.add_facet_field 'building_facet', :label => 'Location'
     config.add_facet_field 'format', :label => 'Format'
     config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
-    config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
+    config.add_facet_field 'topic_facet', :label => 'Topic', :limit => 20 
     config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
     #config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
     #config.add_facet_field 'subject_geo_facet', :label => 'Region' 
