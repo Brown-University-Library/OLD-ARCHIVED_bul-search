78ba200476b9811ce8f135e6437c8d1f971874e7
Ben Cail <benjamin_cail@brown.edu>
2014-09-03 11:29:37 -0400

    add basic settings for email & sms to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,10 @@ BulSearch::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'bul-search@brown.edu'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
