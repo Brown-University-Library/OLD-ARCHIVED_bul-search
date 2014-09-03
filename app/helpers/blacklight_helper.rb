8d40f61fbc99123ef1639e25db8456593ac98b58
Ted Lawless <lawlesst@gmail.com>
2014-09-03 09:58:46 -0400

    Formatting and details display.

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -12,7 +12,7 @@ module BlacklightHelper
 
   #Get links and labels for 856s from the MARC record
   #
-  #Returns a has with links and label text.
+  #Returns a hash with links and label text.
   def access_urls document
     url_value = document['url_fulltext_display']
     url_label = document['url_suppl_display']
@@ -20,4 +20,5 @@ module BlacklightHelper
         return url_label.zip(url_value).map{|l, u| {:label => l, :url => u || "Available online"} }
     end
   end
+
 end
