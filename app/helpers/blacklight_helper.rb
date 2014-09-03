b13ede21dd3ffc1add4e69cd81052f847bb47bc7
Ted Lawless <lawlesst@gmail.com>
2014-09-03 09:12:28 -0400

    Helpers for access urls.

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -4,4 +4,20 @@ module BlacklightHelper
   def application_name
     "Brown University Library Search"
   end
+
+
+  def has_access_urls document
+    return !document['url_fulltext_display'].nil?
+  end
+
+  #Get links and labels for 856s from the MARC record
+  #
+  #Returns a has with links and label text.
+  def access_urls document
+    url_value = document['url_fulltext_display']
+    url_label = document['url_suppl_display']
+    unless url_value.nil?  || url_label.nil?
+        return url_label.zip(url_value).map{|l, u| {:label => l, :url => u || "Available online"} }
+    end
+  end
 end
