97b32a8564c56da43a22a5491f3c9ee14fb15c0f
Ben Cail <benjamin_cail@brown.edu>
2014-09-15 15:12:09 -0400

    remove special handling for prev/next links - not needed anymore

--- app/helpers/bdr_helper.rb
+++ app/helpers/bdr_helper.rb
@@ -10,32 +10,6 @@ module BdrHelper
   end
 
   ##
-  # Extension point for downstream applications
-  # to provide more interesting routing to
-  # documents
-  def bdr_url_for_document doc, options = {}
-    bdr_path doc['pid']
-  end
-
-  ##
-  # Link to the previous document in the current search context
-  def bdr_link_to_previous_document(previous_document)
-    link_opts = bdr_session_tracking_params(previous_document, search_session['counter'].to_i - 1).merge(:class => "previous", :rel => 'prev')
-    link_to_unless previous_document.nil?, raw(t('views.pagination.previous')), bdr_url_for_document(previous_document), link_opts do
-      content_tag :span, raw(t('views.pagination.previous')), :class => 'previous'
-    end
-  end
-
-  ##
-  # Link to the next document in the current search context
-  def bdr_link_to_next_document(next_document)
-    link_opts = bdr_session_tracking_params(next_document, search_session['counter'].to_i + 1).merge(:class => "next", :rel => 'next')
-    link_to_unless next_document.nil?, raw(t('views.pagination.next')), bdr_url_for_document(next_document), link_opts do
-      content_tag :span, raw(t('views.pagination.next')), :class => 'next'
-    end
-  end
-
-  ##
   # Attributes for a link that gives a URL we can use to track clicks for the current search session
   # @param [SolrDocument] document
   # @param [Integer] counter
