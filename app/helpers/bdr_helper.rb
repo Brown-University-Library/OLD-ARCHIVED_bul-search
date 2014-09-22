dec12d739dc1acc917eae96f313384e2b366c9ae
Ben Cail <benjamin_cail@brown.edu>
2014-09-22 11:23:21 -0400

    fix constraints being rendered in bdr advanced search

--- app/helpers/bdr_helper.rb
+++ app/helpers/bdr_helper.rb
@@ -40,4 +40,43 @@ module BdrHelper
 
     content_tag("div", safe_join(content, "\n"), :class=> wrapping_class)
   end
+
+  def bdr_render_constraints(localized_params = params)
+    bdr_render_constraints_query(localized_params) + render_constraints_filters(localized_params)
+  end
+
+  #Over-ride of Blacklight method, provide advanced constraints if needed,
+  # otherwise call super. Existence of an @advanced_query instance variable
+  # is our trigger that we're in advanced mode.
+  def bdr_render_constraints_query(my_params = params)
+    if (@advanced_query.nil? || @advanced_query.keyword_queries.empty? )
+      #return super(my_params)
+      return render_constraints_query(my_params)
+    else
+      content = []
+      @advanced_query.keyword_queries.each_pair do |field, query|
+        label = search_field_def_for_key(field)[:label]
+        if my_params[:controller] == 'bdr'
+          url = bdr_index_path(remove_advanced_keyword_query(field, my_params))
+        else
+          url = catalog_index_path(remove_advanced_keyword_query(field,my_params))
+        end
+        puts "render_constraints_query url: #{url.inspect}"
+        content << render_constraint_element(
+          label, query,
+          :remove => url
+        )   
+      end 
+      if (@advanced_query.keyword_op == "OR" &&
+          @advanced_query.keyword_queries.length > 1)
+        content.unshift content_tag(:span, "Any of:", class:'operator')
+        content_tag :span, class: "inclusive_or appliedFilter well" do
+          safe_join(content.flatten, "\n")
+        end 
+      else
+        safe_join(content.flatten, "\n")        
+      end 
+    end 
+  end
+
 end
