513875cd7cee286a84fd6f85bc94980a27f17454
birkin <birkin.diana@gmail.com>
2014-09-30 09:52:49 -0400

    fixes test-framework.

--- test/controllers/easy_controller_test.rb
+++ test/controllers/easy_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class EasyControllerTest < ActionController::TestCase
-  # test "should get search" do
-  #   get :search
-  #   assert_response :success
-  # end
+  test "should get search" do
+    get :search
+    assert_response :success
+  end
 
 end
