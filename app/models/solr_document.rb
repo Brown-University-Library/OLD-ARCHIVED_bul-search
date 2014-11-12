949ab88b5af5b9268ee717f4f20a977b8752a2aa
Ted Lawless <lawlesst@gmail.com>
2014-11-12 14:34:39 -0500

    Extend Solr::Document::Marc to have a method for subjects.

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -1,15 +1,15 @@
 # -*- encoding : utf-8 -*-
-class SolrDocument 
+class SolrDocument
 
-  include Blacklight::Solr::Document    
+  include Blacklight::Solr::Document
       # The following shows how to setup this blacklight document to display marc documents
   extension_parameters[:marc_source_field] = :marc_display
   extension_parameters[:marc_format_type] = :json
   use_extension( Blacklight::Solr::Document::Marc) do |document|
     document.key?( :marc_display  )
   end
-  
-  field_semantics.merge!(    
+
+  field_semantics.merge!(
                          :title => "title_display",
                          :author => "author_display",
                          :language => "language_facet",
@@ -19,10 +19,10 @@ class SolrDocument
 
 
   # self.unique_key = 'id'
-  
+
   # Email uses the semantic field mappings below to generate the body of an email.
   SolrDocument.use_extension( Blacklight::Solr::Document::Email )
-  
+
   # SMS uses the semantic field mappings below to generate the body of an SMS email.
   SolrDocument.use_extension( Blacklight::Solr::Document::Sms )
 
@@ -31,6 +31,28 @@ class SolrDocument
   # single valued. See Blacklight::Solr::Document::ExtendableClassMethods#field_semantics
   # and Blacklight::Solr::Document#to_semantic_values
   # Recommendation: Use field names from Dublin Core
-  use_extension( Blacklight::Solr::Document::DublinCore)    
+  use_extension( Blacklight::Solr::Document::DublinCore)
+
+  #Local MARC extensions
+  module MarcSubjects
+    include Blacklight::Solr::Document::Marc
+
+    def marc_subjects
+      subjs = []
+      to_marc.find_all {|f| ('600'..'699') === f.tag}.each do |field|
+        txt = []
+        field.each do |sub_field|
+          txt << sub_field.value
+        end
+        subjs << "#{txt.join(" -- ")}"
+      end
+      return subjs
+    end
+
+  end
+
+  use_extension(MarcSubjects) do |document|
+    document.key?( :marc_display )
+  end
 
 end
