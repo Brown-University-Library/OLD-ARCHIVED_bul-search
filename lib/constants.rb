2fb7b205e239250a198c47251b2362790e8c7f6e
Ted Lawless <lawlesst@gmail.com>
2014-11-14 12:58:59 -0500

    Use constants file for icons.

---
+++ lib/constants.rb
@@ -0,0 +1,15 @@
+#User for app wide settings.
+module Constants
+    #Font aweseome icon classes.
+    ICONS = {
+        "Book" => "book",
+        "Computer File" => "file-code-o",
+        "Journal" => "newspaper",
+        "Map" => "globe",
+        "Musical Score" => "music",
+        "Newspaper" => "newspaper",
+        "Periodical Title" => "newspaper",
+        "Sound Recording" => "volume-up",
+        "Video" => "film",
+    }
+end
