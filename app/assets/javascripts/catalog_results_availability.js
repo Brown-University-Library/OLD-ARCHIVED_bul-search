f3d376931c28ad3dfe5ae39c009d68fef23b3884
Ted Lawless <lawlesst@gmail.com>
2014-11-14 16:43:51 -0500

    Remove unused code.

--- app/assets/javascripts/catalog_results_availability.js
+++ app/assets/javascripts/catalog_results_availability.js
@@ -3,8 +3,7 @@
   - Checks that availability info hasn't already been created (addressing a Safari event issue)
   - Gets bib_id
   - Hits availability api.
-  - Determines summary availability.
-  - Builds html & inserts it into dom.
+  - Displays holdings table.
 - Loaded by `app/views/catalog/_search_results.html.erb`.
 */
 
@@ -59,46 +58,6 @@ function getAvailabilityData( the_doc, bib_id ) {
     }
   );
 }
-
-function determineAvailability( response_object, the_doc, bib_id ) {
-  /* Determines item's summarized availability, whether easyBorrow button should display, and triggers html creation.
-   * Called by getAvailabilityData() */
-  // console.log( 'determineAvailability() response_object, ' + JSON.stringify(response_object, undefined, 2) );
-  var availability_status = "unknown"; var show_ezb_button = false; var openurl = null;
-  if (response_object['items'].length > 0 ) {  //check for items before updating HTML.
-    var available_item = _.find(  // _.find() stops processing on first find
-      response_object['items'],
-      function( item ) { if ( item['is_available'] == true ){ return item; } } );
-    if ( ! available_item ) {
-      availability_status = "unavailable"; show_ezb_button = true;
-    } else {
-      availability_status = "available"; show_ezb_button = false;
-    }
-    populateDiv( the_doc, availability_status, show_ezb_button, bib_id );
-  }
-}
-
-function populateDiv( the_doc, availability_status, show_ezb_button, bib_id ) {
-  /* Builds and updates html.
-   * Called by determineAvailability() */
-  class_status = 'status_' + availability_status
-  availability_html = buildAvailabilityHtml( availability_status, class_status, show_ezb_button, bib_id );
-  $( the_doc ).append( availability_html );
-}
-
-function buildAvailabilityHtml( availability_status, class_status, show_ezb_button, bib_id ) {
-  /* Builds availability html.
-   * Called by populateDiv() */
-  context = {
-      'class_status': class_status,
-      'availability_status': availability_status,
-      'show_ezb_button': show_ezb_button,
-      'bib_id': bib_id
-  };
-  availability_html = HandlebarsTemplates['catalog/catalog_results_availability_display'](context);
-  return availability_html;
-}
-
 
 /*
  * On 'Request' button click...
