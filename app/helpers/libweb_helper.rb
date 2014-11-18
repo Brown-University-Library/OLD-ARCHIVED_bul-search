9a74a9bfeadd29d8f72149c120ecd8d0f33818c0
Ted Lawless <lawlesst@gmail.com>
2014-11-18 13:27:21 -0500

    More libweb search tweaks.

--- app/helpers/libweb_helper.rb
+++ app/helpers/libweb_helper.rb
@@ -1,2 +1,9 @@
 module LibwebHelper
+    def libweb_more(query)
+        link_to("More results", libweb_more_link(query))
+    end
+
+    def libweb_more_link(query)
+        url_for :controller=>'libweb', :action=> 'search', :q => query
+    end
 end
