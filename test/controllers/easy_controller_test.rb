7127c8cf81f955155b35bf21a1f1c311d368c6ad
Ted Lawless <lawlesst@gmail.com>
2015-01-22 12:48:56 -0500

    Move catalog_controller test to spec.  Remove autogenerated test dir.

--- test/controllers/easy_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EasyControllerTest < ActionController::TestCase
-  test "should get search" do
-    get :search
-    assert_response :success
-  end
-
-end
--- test/helpers/easy_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class EasyHelperTest < ActionView::TestCase
-end
