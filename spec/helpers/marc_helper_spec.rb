132d32a35c64f72804d9189a53c0b865fb1037c8
Ted Lawless <lawlesst@gmail.com>
2015-01-22 11:48:32 -0500

    Test for render_record_notes partial.

--- spec/helpers/marc_helper_spec.rb
+++ spec/helpers/marc_helper_spec.rb
@@ -3,15 +3,15 @@ require "spec_helper"
 def imprint
     <<-xml
     <record>
-      <datafield tag="245" ind1="1" ind2="0">
+      <datafield tag="245">
         <subfield code="a">As the eye moves ...</subfield>
         <subfield code="c">a sculpture by Henry Moore. Photos. by David Finn. Words by Donald Hall.</subfield>
       </datafield>
-      <datafield tag="500" ind1=" " ind2=" ">
-        <subfield code="a">Photos of Moore&apos;s Bridge-Prop.</subfield>
+      <datafield tag="260">
+        <subfield code="a">A publisher</subfield>
       </datafield>
-      <datafield tag="650" ind1=" " ind2=" ">
-        <subfield code="a">Photos of Moore&apos;s Bridge-Prop.</subfield>
+      <datafield tag="500">
+        <subfield code="a">Photos of Moore</subfield>
       </datafield>
     </record>
     xml
@@ -24,19 +24,33 @@ describe MarcHelper do
     @solrdoc = SolrDocument.new(:marc_display => imprint)
   end
 
-  # describe "#marc_display" do
+  describe "#marc_display" do
 
-  #   it "renders the imprint" do
-  #     val = helper.marc_display(@solrdoc, "imprint")
-  #     expect(val).to eq "stuff"
-  #   end
+    it "renders the imprint" do
+      val = helper.marc_display(@solrdoc, "imprint")
+      expect(val).to eq "A publisher"
+    end
 
-  # end
+  end
 
   describe "#marc_display_tag" do
     it "handles a note" do
       val = helper.marc_display_tag(@solrdoc, "500")[0]
       expect(val).to include("Photos of Moore")
+    end
+  end
+
+  describe "#render_record_notes" do
+    it "renders the right note partial with the expected locals" do
+      note_display = [{:label => "Note", :values => ["Photos of Moore"]}]
+      #http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+      expect(helper).to receive(:render).with(
+        {
+          :partial=>'catalog/record/notes',
+          :locals=>{:note_display => note_display}
+        }
+      )
+      helper.render_record_notes(@solrdoc)
     end
   end
 
