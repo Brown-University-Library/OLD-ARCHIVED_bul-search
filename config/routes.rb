40fd5732e5be75583943e83f3ca18d68f5b9b631
Ted Lawless <lawlesst@gmail.com>
2015-01-30 08:46:02 -0500

    Make easy#home the root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 BulSearch::Application.routes.draw do
-  get "easy/search"
-  root :to => "catalog#index"
+  root "easy#home"
+
+  #catalog
   blacklight_for :catalog
   post 'catalog/sms' => 'catalog#sms'
   Blacklight::Marc.add_routes(self)
   devise_for :users
-
   get 'catalog/:id/ourl' => 'catalog#ourl_service', as: :catalog_service_ourl
 
+  #easySearch
+  get "easy/search"
   get 'easy/' => 'easy#home', as: :easyS
   get 'easy/search'
+
+  #bdr
   get 'bdr/advanced' => 'bdr_advanced#index', as: :bdr_advanced_search
   get 'bdr' => 'bdr#index', as: :bdr_index
   get 'bdr/email' => 'bdr#email', as: :email_bdr
@@ -19,6 +23,7 @@ BulSearch::Application.routes.draw do
   post 'bdr/:id/track' => 'bdr#track', as: :bdr_track
   get 'bdr/:id' => 'bdr#show', as: :bdr_solr_document
 
+  #libweb
   get 'libweb/' => 'libweb#search', as: :lib_web
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
