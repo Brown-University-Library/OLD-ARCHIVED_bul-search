1e62d85283d53795d1612a66127496c3124615d2
Ted Lawless <lawlesst@gmail.com>
2015-04-23 11:56:07 -0400

    Adjust sync script.

--- solr_conf/sync.sh
+++ solr_conf/sync.sh
@@ -7,7 +7,7 @@ set -x
 #SOLR_HOME
 #SOLR_URL
 #CORE
-rsync -avz solr/$SOLR_CORE/ $SOLR_HOME/$SOLR_CORE/
+rsync -avz solr_conf/$SOLR_CORE/ $SOLR_HOME/$SOLR_CORE/
 
 #http://localhost:8983/solr/admin/cores?action=RELOAD&core=core0
-curl "$SOLR_URL/admin/cores?action=RELOAD&core=$SOLR_CORE"
+curl "$SOLR_BASE_URL/admin/cores?action=RELOAD&core=$SOLR_CORE"
