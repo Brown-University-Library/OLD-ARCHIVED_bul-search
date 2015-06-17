9f7518722fb00493936e8e63f068d9e5907c5a65
Ted Lawless <lawlesst@gmail.com>
2015-06-17 11:56:12 -0400

    Pull secret key from env.

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module BulSearch
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.secret_key_base = 'blight'
+    config.secret_key_base = ENV['ESEARCH_KEY']
   end
 end
