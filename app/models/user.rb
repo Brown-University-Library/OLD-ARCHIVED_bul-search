d030f025f112a515c1021346a80031b843d51ba0
Ted Lawless <lawlesst@gmail.com>
2015-06-30 10:37:19 -0400

    Fix Shib auth related to strong parameters in rails 4.1.4.  See: http://stackoverflow.com/questions/25399414/rails-4-1-5-omniauth-strong-parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :omniauthable, :validatable
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
