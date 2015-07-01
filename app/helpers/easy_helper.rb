5405f4c69a631fc98e6451415a03ffe72149b2f5
Ted Lawless <lawlesst@gmail.com>
2015-07-01 08:14:32 -0400

    Remove redundant helper; add it's method to easy_helper.

--- app/helpers/easy_helper.rb
+++ app/helpers/easy_helper.rb
@@ -61,4 +61,21 @@ module EasyHelper
     render partial: "best_bet", locals: bet
   end
 
+  #
+  #Link back to easySearch with the last search if available.
+  #Returns a url string to the easy controller.
+  def easy_search_link
+    #Get the search from the history if it's there or from q param.
+    query = get_last_easy_search || params[:q] || @current_search_session.query_params[:q] if @current_search_session.respond_to? :query_params
+    if query.nil?
+      return url_for :controller=>'easy', :action=> 'home'
+    else
+      return url_for :controller=>'easy', :action=> 'home', :q => query
+    end
+  end
+
+  def get_last_easy_search
+    session[:last_easy_search] || nil
+  end
+
 end
