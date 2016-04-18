d3209dfe9e38bcc6478b08ef3e8e0e3a2fc25ae6
Ben Cail <benjamin_cail@brown.edu>
2016-04-18 14:29:59 -0400

    handle display and searching of uniform title with searching of author field

--- app/helpers/marc_helper.rb
+++ app/helpers/marc_helper.rb
@@ -16,6 +16,19 @@ module MarcHelper
     link_to(query, link_url)
   end
 
+  def uniform_title_author_search(query, author)
+    #Remove > for links and replace with blank.
+    q = query.gsub(/>|--/, '')
+    if ! q.start_with? '"'
+      q = "\"#{q}\""
+    end
+    params = {:controller => "catalog", :action => 'index', :search_field => 'advanced'}
+    params['uniform_title_search_facet'] = q
+    params['author'] = author
+    link_url = search_action_path(params)
+    link_to(query, link_url)
+  end
+
   def hot_link(terms, index)
     out = []
     terms.each do |term|
