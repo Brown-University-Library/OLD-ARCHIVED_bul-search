d8b4e287a0574f2bb659932282f16e91c08f3ce8
Ben Cail <benjamin_cail@brown.edu>
2016-06-27 11:00:29 -0400

    update refresh.sh script with some chown commands (note: need to address db:migrate at some point)

--- refresh.sh
+++ refresh.sh
@@ -3,4 +3,6 @@ set -e
 git pull
 RAILS_ENV=production bundle install --without development test
 RAILS_ENV=production bundle exec rake assets:precompile
+sudo chown -R blacklight.libstaff /opt/local/bul-search-src/public/assets/
+sudo chown -R blacklight.libstaff /opt/local/bul-search-src/tmp/
 touch tmp/restart.txt
