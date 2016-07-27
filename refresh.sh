34e901f9ff33c8ddc25a4f094687855c2b6daa92
Hector Correa <hector_correa@brown.edu>
2016-07-27 09:46:41 -0400

    Added db:migrate step and commented chown calls now that we are deploying with a deploy user

--- refresh.sh
+++ refresh.sh
@@ -2,7 +2,8 @@
 set -e
 git pull
 RAILS_ENV=production bundle install --without development test
+RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:precompile
-sudo chown -R blacklight.libstaff /opt/local/bul-search-src/public/assets/
-sudo chown -R blacklight.libstaff /opt/local/bul-search-src/tmp/
+# sudo chown -R blacklight.libstaff /opt/local/bul-search-src/public/assets/
+# sudo chown -R blacklight.libstaff /opt/local/bul-search-src/tmp/
 touch tmp/restart.txt
