bc4e87d7f3a57533754285ead51cf9002f983651
Hector Correa <hector_correa@brown.edu>
2016-08-16 13:36:47 -0400

    Renders new Josiah links for items with online links pointing to classic Josiah

--- app/models/online_avail_data.rb
+++ app/models/online_avail_data.rb
@@ -1,11 +1,18 @@
 class OnlineAvailData
   attr_reader :url, :label
 
+  CLASSIC_JOSIAH_URL = "http://josiah.brown.edu/record="
+  NEW_JOSIAH_URL = "http://search.library.brown.edu/catalog/"
+
   # url is 856 u
   # note is 856 z
   # materials is 856 z
   def initialize(url, note, materials)
-    @url = url
+    if url.start_with?(CLASSIC_JOSIAH_URL)
+      @url = url.gsub(CLASSIC_JOSIAH_URL, NEW_JOSIAH_URL)
+    else
+      @url = url
+    end
     if note == nil && materials == nil
       @label = "Available online"
     elsif note == nil && materials != nil
