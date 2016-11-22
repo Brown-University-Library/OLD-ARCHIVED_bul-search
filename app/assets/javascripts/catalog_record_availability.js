e723718b8299276051a39f16456cb1b81033c65c
Hector Correa <hector_correa@brown.edu>
2016-11-22 09:57:50 -0500

    Scroll page when clicking on Virtual Shelf, but not when loading the page initially

--- app/assets/javascripts/catalog_record_availability.js
+++ app/assets/javascripts/catalog_record_availability.js
@@ -1,6 +1,5 @@
 // JavaScript functions for individual catalog records.
 // Loaded by `app/views/catalog/show.html.erb`.
-
 $(document).ready(
   function(){
 
@@ -17,27 +16,30 @@ $(document).ready(
     $.getJSON(api_url, addAvailability);
 
     if (location.search.indexOf("nearby") > -1) {
-      findNearbyItems(bib_id);
+      findNearbyItems(bib_id, false);
     }
   }
 );
 
+
 function getBibId() {
-  /* Pulls bib_id from DOM.
-   * Called on doc.ready */
+  /* Pulls bib_id from DOM, called on doc.ready */
   bib_id_div_name = $( "div[id^='doc_']" )[0].id;
   bib_id_start = bib_id_div_name.search( '_' ) + 1;
   bib_id = bib_id_div_name.substring( bib_id_start );
   return bib_id;
 }
 
+
 function getTitle() {
   return $('h3[itemprop="name"]').text();
 }
 
+
 function getFormat() {
   return $("dd.blacklight-format").text().trim();
 }
+
 
 function processItems(availabilityResponse) {
   var out = []
@@ -49,9 +51,11 @@ function processItems(availabilityResponse) {
   return rsp;
 }
 
+
 function hasItems(availabilityResponse) {
   return (availabilityResponse.items.length > 0);
 }
+
 
 function addAvailability(availabilityResponse) {
   var title = getTitle();
@@ -96,15 +100,16 @@ function addAvailability(availabilityResponse) {
   if (availabilityResponse.requestable) {
     context['request_link'] = requestLink();
   };
-  //context['openurl'] = openurl
   html = HandlebarsTemplates['catalog/catalog_record_availability_display'](context);
   $("#availability").append(html);
 }
+
 
 function browseShelfUri(id) {
   // josiahRootUrl is defined in shared/_header_navbar.html.erb
   return josiahRootUrl + "api/items/nearby?id=" + id;
 }
+
 
 function callnumbers_text(callnumbers) {
   if (!callnumbers) {
@@ -121,22 +126,25 @@ function callnumbers_text(callnumbers) {
   return text;
 }
 
+
 function loadNearbyItems() {
   id = getBibId();
-  findNearbyItems(id);
+  findNearbyItems(id, true);
 }
 
-function findNearbyItems(id) {
+
+function findNearbyItems(id, scrollPage) {
   $.ajax({
       type: "GET",
       url: browseShelfUri(id),
       success: function(data) {
-        renderNearbyItems(data.documents);
+        renderNearbyItems(data.documents, scrollPage);
       }
   });
 }
 
-function renderNearbyItems(items) {
+
+function renderNearbyItems(items, scrollPage) {
   var i, doc, link;
   var docs = [];
   for(i = 0; i < items.length; i++) {
@@ -154,10 +162,11 @@ function renderNearbyItems(items) {
     };
     docs.push(doc);
   }
-  renderStackView(docs);
+  renderStackView(docs, scrollPage);
 }
 
-function renderStackView(docs) {
+
+function renderStackView(docs, scrollPage) {
   var data = {
     "start": "-1",
     "limit": "0",
@@ -165,15 +174,19 @@ function renderStackView(docs) {
     "docs":docs
   };
   $('#basic-stack').stackView({data: data, ribbon: null});
-  // scroll to bottom of the page
-  // http://stackoverflow.com/a/10503637/446681
-  $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+  if (scrollPage) {
+    // scroll to bottom of the page
+    // http://stackoverflow.com/a/10503637/446681
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+  }
 }
+
 
 function requestLink() {
   var bib = getBibId();
   return 'https://josiah.brown.edu/search~S7?/.' + bib + '/.' + bib + '/%2C1%2C1%2CB/request~' + bib;
 }
+
 
 function addRequestButton(availabilityResponse) {
   //ugly Josiah request url.
@@ -198,32 +211,4 @@ function getUrlParameter(sParam)
             return sParameterName[1];
         }
     }
-}
-
-function requestBookcovers(isbns) {
-  var booksApiUrl = '//books.google.com/books?jscmd=viewapi&bibkeys=' + isbns;
-
-  $.ajax({
-    type: 'GET',
-    url: booksApiUrl,
-    async: false,
-    contentType: "application/json",
-    dataType: 'jsonp',
-
-    success: function(json) {
-      $.each(json, function(id, data) {
-        if (data.thumbnail_url != undefined) {
-          var thumbUrl = data.thumbnail_url;
-          thumbUrl = thumbUrl.replace(/zoom=5/, 'zoom=1');
-          thumbUrl = thumbUrl.replace(/&?edge=curl/, '');
-          var imgEl = $("#" + id);
-          imgEl.attr('src', thumbUrl);
-        }
-      });
-    },
-    error: function(e) {
-      console.log(e);
-    }
-  });
-
 }
