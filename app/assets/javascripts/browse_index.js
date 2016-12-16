4a9037060ad1d8363fc846e085434701a057c6c5
Hector Correa <hector_correa@brown.edu>
2016-12-16 10:43:09 -0500

    Added book covers

--- app/assets/javascripts/browse_index.js
+++ app/assets/javascripts/browse_index.js
@@ -35,15 +35,75 @@ $(document).ready(function(){
 });
 
 function showPreview(data) {
+  // TODO: handle undefined properties
   var doc = data.response.document;
   var uri = josiahRootUrl + "catalog/" + doc.id;
   $("#previewTitle").html(doc.title_display);
   $("#previewAuthor").html(doc.author_display);
   $("#previewPubDate").html(doc.pub_date[0]);
   $("#previewFormat").html(doc.format);
-  $("#previewIsbn").html(doc.isbn_t.toString());
-  $("#previewCallnumbers").html(doc.callnumber_t.toString());
-  $("#previewLink").attr("href", uri);
-  $("#previewLinkImage").attr("href", uri);
+  // $("#previewIsbn").html(doc.isbn_t.toString());
+  // $("#previewCallnumbers").html(doc.callnumber_t.toString());
   $("#previewPanel").removeClass("hidden");
+
+  $("#previewLink").attr("href", uri)
+  $("#previewLinkImage").attr("href", uri)
+
+  var keys = bookCoverKeys(doc.isbn_t, doc.oclc_t);
+  loadBookCover(keys);
+}
+
+function bookCoverKeys(isbn_t, oclt_t) {
+  var i;
+  var keys = "";
+  if (isbn_t != undefined) {
+    for(i = 0; i < isbn_t.length; i++) {
+      keys += "ISBN" + isbn_t[i] + ",";
+    }
+  }
+  if (oclt_t != undefined) {
+    for(i = 0; i < oclt_t.length; i++) {
+      keys += "OCLC" + oclt_t[i] + ",";
+    }
+  }
+  return keys;
+}
+
+function loadBookCover(keys) {
+  var booksApiUrl = "https://books.google.com/books?jscmd=viewapi&bibkeys=" + keys;
+  $.ajax({
+    type: 'GET',
+    url: booksApiUrl,
+    async: false,
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json) {
+      var i, key, obj;
+      var found = false;
+      var keys = Object.keys(json);
+      for(i = 0; i < keys.length; i++) {
+        key = keys[i];
+        obj = json[key];
+        if (obj.thumbnail_url != undefined) {
+          $("#previewImage").attr("alt", "Book cover");
+          $("#previewImage").attr("src", obj.thumbnail_url);
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        resetBookCover();
+      }
+    },
+    error: function(e) {
+      console.log(e);
+      resetBookCover();
+    }
+  });
+}
+
+function resetBookCover() {
+  var noCover = $("#previewImageDefault").attr("src");
+  $("#previewImage").attr("alt", "No book cover available");
+  $("#previewImage").attr("src", noCover);
 }
