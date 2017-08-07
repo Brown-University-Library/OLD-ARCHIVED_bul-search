f6409850ece94a6043566f5e5e1b7472a1ac9096
Hector Correa <hector_correa@brown.edu>
2017-08-07 16:46:39 -0400

    Removed unnecessary changes now that I figured out the issue with fontawesome

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# require 'font-awesome-rails'
-
 # Initialize the Rails application.
 BulSearch::Application.initialize!
-
-require 'font-awesome-rails'
