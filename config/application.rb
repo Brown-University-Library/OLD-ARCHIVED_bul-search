1f45bcb33c11eeb05c2ad5718911a18d5611e72a
Hector Correa <hector_correa@brown.edu>
2017-08-07 10:13:30 -0400

    Trying to fix issue with loading of font-awesome within the Rails asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 require 'rails/all'
+require 'font-awesome-rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
