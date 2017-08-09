672d6a2294178e3b8a19f3c21169c1bba6860d97
Hector Correa <hector_correa@brown.edu>
2017-08-09 11:32:51 -0400

    Handle call numbers with a copy value embedded (e.g. c.2)

--- app/models/reserves.rb
+++ app/models/reserves.rb
@@ -95,12 +95,23 @@ class ReservesBook
       # only use the bib number if it looks like a bib number
       rb.bib = hash["bib"]
     end
-    rb.callno = hash["callno"]
+    rb.callno = (hash["callno"] || "").strip
     rb.title = hash["title"]
     rb.author = hash["author"]
     rb.loan_term = hash["loan_term"]
     rb.personal = hash["personal"] # item is the personal copy of the professor
     rb
+  end
+
+  def callno_search
+    return nil if callno == nil
+    search_value = callno.strip
+    index = (search_value =~ /\w* c\.\d+/)
+    if index != nil
+      # strip the copy value (e.g. c.2 or c.3)
+      search_value = (search_value[0..(index-1)] || "").strip
+    end
+    search_value
   end
 end
 
