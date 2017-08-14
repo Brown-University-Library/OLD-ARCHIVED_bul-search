412576280f64ae3fc93bfccd876d34b63df841da
Hector Correa <hector_correa@brown.edu>
2017-08-14 13:34:14 -0400

    Handle course not found errors

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -53,9 +53,27 @@ class ReservesController < ApplicationController
   end
 
   def show
-    @classid = params[:classid]
-    reserves = Reserves.new
-    @materials = reserves.items_for_course(@classid)
-    @page_title = "Reserves #{@materials.course.number_section}"
+    begin
+      @classid = params[:classid]
+      @classnumber = params[:classnumber]
+      reserves = Reserves.new
+      @materials = reserves.items_for_course(@classid)
+      @page_title = "Reserves #{@materials.course.number_section}"
+      render
+    rescue StandardError => e
+      @page_title = "Reserves #{@classnumber}"
+      if e.class == JSON::ParserError && e.message.include?("not found")
+        status = 404
+        @not_found_title = "Course #{@classnumber} was not found"
+        @not_found_msg = "There is no information for course #{@classnumber} (ID: #{@classid})"
+      else
+        Rails.logger.error("Course Reserves: #{e.message}")
+        Rails.logger.error(e.backtrace.join("\n"))
+        status = 500
+        @not_found_title = "Error getting details for course #{@classnumber}"
+        @not_found_msg = "Error getting information for course #{@classnumber} (ID: #{@classid})"
+      end
+      render "show_error", status: status
+    end
   end
 end
