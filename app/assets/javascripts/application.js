378bb371961c38ef758cc9b722665e4bd1723a60
Hector Correa <hector_correa@brown.edu>
2017-08-23 09:47:48 -0400

    Disable Aeon code until we are ready

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -154,6 +154,8 @@ function hayAeonFullLink( bib, title, author, publisher, callnumber ) {
 }
 
 function isValidHayAeonLocation( josiah_location ) {
+  // TODO: Enable this once we go live with Aeon.
+  return false;
   /* called by catalog_record_availability.js */
   console.log( '- starting isValidHayAeonLocation()' )
   var hay_found = false;
