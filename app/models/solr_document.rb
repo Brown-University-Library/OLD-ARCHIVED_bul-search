35126b484f9eb7ef4163f285cd06d6d064b04315
Hector Correa <hector_correa@brown.edu>
2018-01-24 11:55:07 -0500

    Moved table of contents class to its own file

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -2,37 +2,7 @@
 require 'bulmarc'
 require 'json'
 require 'open-uri'
-
-class TableOfContents
-
-  def initialize toc_970_display, toc_display
-    if !toc_970_display.nil?
-      @toc_info = JSON.parse(toc_970_display[0])
-    elsif !toc_display.nil?
-      @toc_info = JSON.parse(toc_display[0])
-    else
-      raise Exception.new('no TableOfContents info')
-    end
-    @chapters = make_chapters
-  end
-
-  def make_chapters
-    chapters = []
-    @toc_info.each do |chapter|
-      ['label', 'indent', 'title', 'page'].each do |key|
-        chapter[key] = "" if chapter[key].nil?
-      end
-      chapter['authors'] = [] if chapter['authors'].nil?
-      chapters << chapter
-    end
-    chapters
-  end
-
-  def chapters
-    @chapters
-  end
-
-end
+require "./app/models/table_of_contents.rb"
 
 class SolrDocument
 
