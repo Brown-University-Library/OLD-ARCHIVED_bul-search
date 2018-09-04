0c1642eaaae17ff00159a6a5ba9c533365217e7c
Hector Correa <hector_correa@brown.edu>
2018-09-04 14:47:38 -0400

    Log the IP address of invalid requests

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -253,7 +253,7 @@ class CatalogController < ApplicationController
     adjust_special_fields()
 
     if invalid_search()
-      Rails.logger.info("Skipped invalid search: #{params.keys}")
+      Rails.logger.info("Skipped invalid search for #{request.ip}: #{params.keys}")
       render text: "invalid request", status: 400
       return
     end
