2e458b94633a6bbe0a517e82e79cc310bb1c269e
Hector Correa <hector_correa@brown.edu>
2018-09-24 12:47:26 -0400

    Better handling of wildcards, moved the call number functions to the SearchCustom class, closer to their use

--- app/models/search_custom.rb
+++ app/models/search_custom.rb
@@ -13,16 +13,23 @@ class SearchCustom
   #   match: The callnumber that was found (could be different from the one requested)
   def callnumber(callnumber, params)
     params = params || {}
-    callnumber_searchable = StringUtils.callnumber_searchable(callnumber)
+    search_term = callnumber_searchable(callnumber)
     solr_query = SolrQuery.new(@blacklight_config)
-    if callnumber_searchable == ""
+    if search_term == ""
       q = "*:*"
     else
-      q =  "callnumber_ss:#{callnumber_searchable}"
+      q =  "callnumber_ss:#{search_term}"
     end
+
     response, docs = solr_query.search(q, params)
     if docs.count > 0
-      Rails.logger.info("Call number search success: #{callnumber}")
+      # We are done
+      return response, docs, callnumber
+    end
+
+    byebug
+    if wildcard_search?(callnumber)
+      # Don't retry wildcard searches
       return response, docs, callnumber
     end
 
@@ -31,19 +38,80 @@ class SearchCustom
     # https://search.library.brown.edu/catalog/b2340347
     # Notice that the "33rd" in the call number "1-SIZE GN33 .G85 1994/1995 33rd"
     # is not in the MARC data that we get from Sierra.
-    shortened = StringUtils.call_number_shorten(callnumber)
+    shortened = callnumber_shorten(callnumber)
     if shortened == ""
-      # nothing else to try
+      # Nothing else to try
       return response, docs, callnumber
     end
 
-    # try with the shortened callnumber
     q =  "callnumber_ss:#{shortened}"
     response, docs = solr_query.search(q, params)
     if docs.count == 0
       return response, docs, callnumber
     end
 
-    return response, docs, StringUtils.drop_quotes(shortened)
+    return response, docs, callnumber_human(shortened)
   end
+
+  private
+    # Returns the text in a format suitable for call number search.
+    def callnumber_searchable(text)
+      # Drop the N-SIZE prefix since we don't index it.
+      text = text.strip.gsub(/\d-SIZE\s/,"")
+      if wildcard_search?(text)
+        # Make it a Solr RegEx value
+        text = "/" + solr_safe_regex(text.gsub("*", "")) + ".*/"
+      else
+        # Surround the value in quotes
+        text = '"' + text + '"'
+      end
+      text
+    end
+
+    # Shorten a call number by dropping the last token
+    def callnumber_shorten(text)
+      tokens = text.split(" ")
+      return "" if tokens.count < 2
+      shorten = tokens[0..-2].join(" ") # drop the last token
+      if wildcard_search?(text)
+        shorten += "*"
+      end
+      callnumber_searchable(shorten)
+    end
+
+    # Returns the value in a human friendly way (i.e. removes the values added
+    # in callnumber_searchable())
+    def callnumber_human(text)
+      if text.length > 2 && text[0] == '"' && text[-1] == '"'
+        # Drop the quotes
+        return text[1..-2]
+      end
+      if text.length > 4 && text[0] == '/' && text.end_with?('.*/')
+        # Drop the Solr RegEx markers
+        return text[1..-4] + "*"
+      end
+      text || ""
+    end
+
+    def wildcard_search?(text)
+      return text.strip.end_with?("*")
+    end
+
+    # TODO: move this to a shared class so it's not duplicated with
+    # bookplate_regex in catalog_controller.rb
+    def solr_safe_regex(value)
+      safe_value = ""
+      value.each_char do |c|
+        case
+          when (c >= "a" && c <= "z") || (c >= "A" && c <= "Z") ||
+            (c >= "0" && c <= "9") || c == " " || c == "_"
+            safe_value += c
+          when c == "+" || c == "." || c == "*" || c == "/"
+            safe_value += "\\" + c
+          else
+            safe_value += "."
+        end
+      end
+      safe_value
+    end
 end
