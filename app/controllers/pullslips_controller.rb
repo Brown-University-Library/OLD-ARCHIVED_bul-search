79925b2a26a4e03d0a75dc942280833fff51abd2
Hector Correa <hector_correa@brown.edu>
2019-11-14 14:59:27 -0500

    Moved pull slips functionality to its own model class, adjusted the display and print to account for new columns and proper print order

--- app/controllers/pullslips_controller.rb
+++ app/controllers/pullslips_controller.rb
@@ -8,86 +8,30 @@ class PullslipsController < ApplicationController
 
   def show
     @barcode_font = true
-    @id = params["id"]
-    if @id.to_i == 0
-      Rails.logger.error("Invalid list ID received #{@id}")
-      @results = []
-    else
-      @results = hay_data(@id)
+    id = params["id"]
+    if id.to_i == 0
+      Rails.logger.error("Invalid list ID received #{id}")
+      raise "Invalid id"
     end
+    @ps = Pullslips.new(id)
     case
     when params["format"] == "json"
-      render :json => @results
+      render :json => @ps.data
     when params["format"] == "tsv"
-      send_data(to_tsv(@results), :filename => "flags.tsv", :type => "text/tsv")
+      send_data(@ps.to_tsv(), :filename => "flags.tsv", :type => "text/tsv")
     else
       render
     end
   end
 
   def print
-    @id = params["id"]
-    if @id.to_i == 0
-      Rails.logger.error("Invalid list ID received #{@id}")
-      @results = []
-    else
-      @results = hay_data(@id)
+    id = params["id"]
+    if id.to_i == 0
+      Rails.logger.error("Invalid list ID received #{id}")
+      raise "Invalid id"
     end
-    @table = data_to_table(@results)
+    @ps = Pullslips.new(id)
+    @table = @ps.to_print_table()
     render layout: false
   end
-
-  def hay_flags
-  end
-
-  private
-    def hay_data(id)
-        Rails.cache.fetch("pullslips_#{id}", expires_in: 20.minute) do
-            url = ENV["BIB_UTILS_SERVICE"] + "/bibutils/pullSlips?id=#{id}"
-            Rails.logger.info("Loading Pull Slips from bibService #{url}")
-            HttpUtil::HttpJson.get(url, [], 300)
-        end
-    end
-
-    def to_tsv(data)
-      header = data[0].keys.join("\t")
-      tsv = header + "\tJosiahLink\r\n"
-      data.each do |row|
-        values = []
-        row.keys.each do |key|
-          values << row[key]
-        end
-        values << "http://search.library.brown.edu/catalog/#{row['BibRecordNum']}"
-        tsv += values.join("\t") + "\r\n"
-      end
-      tsv
-    end
-
-    def data_to_table(data)
-      data.each do |item|
-        # Make the barcode readable by our scanners
-        # (no spaces, surround with asterisks)
-        item["BarCode"] = "*" + (item["BarCode"] || "").gsub(" ", "") + "*"
-      end
-
-      # Force to length of the array be a multiple of 3
-      # (simplifies the algorithm of dumping them into a table with 3 columns)
-      mod = (data.count % 3)
-      if mod == 1
-        data << nil
-        data << nil
-      elsif mod == 2
-        data << nil
-      end
-
-      pages = data.count / 3
-      table = []
-      (1..pages).each do |page|
-        i = (page-1) * 3
-        row = [data[i], data[i+1], data[i+2]]
-        table << row
-      end
-
-      table
-    end
 end
---
+++ app/models/pullslips.rb
@@ -0,0 +1,72 @@
+class Pullslips
+    attr_reader :data, :id
+
+    def initialize(id)
+        @id = id
+        @data = fetch_data(id)
+    end
+
+    def title
+        return "" if @data.count == 0
+        @data[0]["ProjectTitle"]
+    end
+
+    def list_date
+        return "" if @data.count == 0
+        @data[0]["ListDate"]["Time"]
+    end
+
+    def fetch_data(id)
+        Rails.cache.fetch("pullslips_#{id}", expires_in: 20.minute) do
+            url = ENV["BIB_UTILS_SERVICE"] + "/bibutils/pullSlips?id=#{id}"
+            Rails.logger.info("Loading Pull Slips from bibService #{url}")
+            data = HttpUtil::HttpJson.get(url, [], 300)
+            data.each_with_index do |item, ix|
+              item["PrintOrder"] = ix + 1
+              item["CallNumber"] = (item["CallNumber"] || "").upcase
+              # Make the barcode readable by our scanners
+              # (no spaces, surround with asterisks)
+              item["BarCodePrint"] = "*" + (item["BarCode"] || "").gsub(" ", "") + "*"
+            end
+            data
+        end
+    end
+
+    def to_tsv()
+      header = @data[0].keys.join("\t")
+      tsv = header + "\tJosiahLink\r\n"
+      @data.each do |row|
+        values = []
+        row.keys.each do |key|
+          values << row[key]
+        end
+        values << "http://search.library.brown.edu/catalog/#{row['BibRecordNum']}"
+        tsv += values.join("\t") + "\r\n"
+      end
+      tsv
+    end
+
+    def to_print_table()
+      # Force to length of the array be a multiple of 3
+      # (simplifies the algorithm of dumping them into a table with 3 columns)
+      mod = (@data.count % 3)
+      if mod == 1
+        @data << nil
+        @data << nil
+      elsif mod == 2
+        @data << nil
+      end
+
+      pages = @data.count / 3
+      table = []
+      (1..pages).each do |page|
+        c1 = (page - 1)
+        c2 = (pages * 1) + (page - 1)
+        c3 = (pages * 2) + (page - 1)
+        row = [@data[c1], @data[c2], @data[c3]]
+        table << row
+      end
+
+      table
+    end
+end
